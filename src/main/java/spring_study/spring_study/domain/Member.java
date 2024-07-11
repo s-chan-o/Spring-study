80c4e1236f31dc8e510d73ae526ffbc3a94ec7c4
seungchan08 <s24041@gsm.hs.kr>
2024-07-11 20:13:06 +0900

    spring-study

---
+++ src/main/java/spring_study/spring_study/domain/Member.java
@@ -0,0 +1,17 @@
+package spring_study.spring_study.domain;
+
+public class Member {
+
+    private long id;
+    private String name;
+
+    public long getId(){
+        return id;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+}
---
+++ src/main/java/spring_study/spring_study/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package spring_study.spring_study.repository;
+
+import spring_study.spring_study.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
