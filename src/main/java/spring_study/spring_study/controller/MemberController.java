293f781fd70f071fe22215bfe6b6c874980f62ba
김승찬 <s24041@gsm.hs.kr>
2024-09-23 16:01:56 +0900

    spring-study

--- src/main/java/spring_study/spring_study/controller/MemberController.java
+++ src/main/java/spring_study/spring_study/controller/MemberController.java
@@ -3,6 +3,8 @@ package spring_study.spring_study.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import spring_study.spring_study.domain.Member;
 import spring_study.spring_study.service.MemberService;
 
 @Controller
@@ -15,8 +17,19 @@ public class MemberController {
     public MemberController(MemberService memberService){
         this.memberService = memberService;
     }
+
     @GetMapping("/members/new")
     public String createForm(){
-        return "members/createForm";
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:";
     }
 }
