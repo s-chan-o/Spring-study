9c4cb79eef39e5da20820961274a5586c6edae6b
김승찬 <s24041@gsm.hs.kr>
2024-09-23 15:56:47 +0900

    spring-study

--- src/main/java/spring_study/spring_study/controller/MemberController.java
+++ src/main/java/spring_study/spring_study/controller/MemberController.java
@@ -2,6 +2,7 @@ package spring_study.spring_study.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import spring_study.spring_study.service.MemberService;
 
 @Controller
@@ -13,5 +14,9 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createForm";
     }
 }
---
+++ src/main/java/spring_study/spring_study/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package spring_study.spring_study.controller;
+
+public class MemberForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
